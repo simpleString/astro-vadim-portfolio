e8ea3086c37eb48a2fa339e5641e9c5b26ed5784
Volkov Dmitry <hot-gun@mail.ru>
2022-09-20 03:29:48 +0700

    Save changes for today

---
+++ src/components/ModalModel.tsx
@@ -0,0 +1,5 @@
+const ModalModel: React.FC = () => {
+  return <div></div>;
+};
+
+export default ModalModel;
--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,7 +1,159 @@
-import type React from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { SpinnerCircular } from "spinners-react";
+import { loadGLTFModel } from "../utils/loadGLTFModel";
+import ModalModel from "./ModalModel";
 
-const Model: React.FC = () => {
-  return <div>Hello from react in astro</div>;
+function easeOutCirc(x: number) {
+  return Math.sqrt(1 - Math.pow(x - 1, 4));
+}
+
+const Model = ({ modelName }: { modelName: string }) => {
+  const refContainer = useRef<HTMLElement>();
+  const [loading, setLoading] = useState(true);
+  const refRenderer = useRef<any>();
+
+  const [isShowZoomButton, setIsShowZoomButton] = useState(false);
+  const [showModal, setShoModal] = useState(false);
+
+  const hideZoomButton = useCallback(() => {
+    setIsShowZoomButton(false);
+  }, []);
+
+  const showZoomButton = useCallback(() => {
+    setIsShowZoomButton(true);
+  }, []);
+
+  const showModalZoom = useCallback(() => {
+    setShoModal(true);
+  }, []);
+
+  const handleWindowResize = useCallback(() => {
+    const { current: renderer } = refRenderer;
+    const { current: container } = refContainer;
+    if (container && renderer) {
+      const scW = container.clientWidth;
+      const scH = container.clientHeight;
+
+      renderer.setSize(scW, scH);
+    }
+  }, []);
+
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(() => {
+    const { current: container } = refContainer;
+    if (container) {
+      const scW = container.clientWidth;
+      const scH = container.clientHeight;
+
+      const renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true,
+      });
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(scW, scH);
+      renderer.outputEncoding = THREE.sRGBEncoding;
+      container.appendChild(renderer.domElement);
+      refRenderer.current = renderer;
+      const scene = new THREE.Scene();
+
+      const target = new THREE.Vector3(-0.5, 1.2, 0);
+      const initialCameraPosition = new THREE.Vector3(
+        20 * Math.sin(0.2 * Math.PI),
+        10,
+        20 * Math.cos(0.2 * Math.PI)
+      );
+
+      // 640 -> 240
+      // 8   -> 6
+      const scale = scH * 0.005 + 4.8;
+      const camera = new THREE.OrthographicCamera(
+        -scale,
+        scale,
+        scale,
+        -scale,
+        0.01,
+        50000
+      );
+      camera.position.copy(initialCameraPosition);
+      camera.lookAt(target);
+
+      const ambientLight = new THREE.AmbientLight(0xcccccc, 1);
+      scene.add(ambientLight);
+
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.autoRotate = true;
+      controls.target = target;
+
+      loadGLTFModel(scene, modelName, {
+        receiveShadow: false,
+        castShadow: false,
+      }).then(() => {
+        animate();
+        setLoading(false);
+      });
+
+      let req: number;
+      let frame = 0;
+      const animate = () => {
+        req = requestAnimationFrame(animate);
+
+        frame = frame <= 100 ? frame + 1 : frame;
+
+        if (frame <= 100) {
+          const p = initialCameraPosition;
+          const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20;
+
+          camera.position.y = 10;
+          camera.position.x =
+            p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed);
+          camera.position.z =
+            p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed);
+          camera.lookAt(target);
+        } else {
+          controls.update();
+        }
+
+        renderer.render(scene, camera);
+      };
+
+      return () => {
+        cancelAnimationFrame(req);
+        renderer.domElement.remove();
+        renderer.dispose();
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize, false);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize, false);
+    };
+  }, [handleWindowResize]);
+
+  return (
+    <>
+      <div
+        className="w-full h-52 relative"
+        ref={refContainer}
+        onMouseOver={showZoomButton}
+        onMouseLeave={hideZoomButton}
+      >
+        {<SpinnerCircular className="w-full h-full m-auto" enabled={loading} />}
+        {/* {isShowZoomButton && (
+          <button
+            onClick={showModalZoom}
+            className="btn absolute mx-auto bottom-4 block w-full"
+          >
+            It's a simple button
+          </button>
+        )} */}
+      </div>
+      {/* {showModal && <ModalModel />} */}
+    </>
+  );
 };
 
 export default Model;
